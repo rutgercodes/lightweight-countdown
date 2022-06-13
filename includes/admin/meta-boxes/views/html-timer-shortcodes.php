1a9c532357f79d0b97a63ca2a82f842b0abd1cc7
Rutger van Wijngaarden <rutgervw@gmail.com>
2022-06-13 10:57:09 +0200

    Better escape option

--- includes/admin/meta-boxes/views/html-timer-shortcodes.php
+++ includes/admin/meta-boxes/views/html-timer-shortcodes.php
@@ -16,13 +16,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<p><?php esc_html_e( 'Use the shortcodes below to display the timer or conditional content based on the timers deadline.', 'lightweight-countdown' ); ?></p>
 
 	<h4><?php esc_html_e( 'Countdown timer text', 'lightweight-countdown' ); ?></h4>
-	<code>[lwcd_timer id="<?php echo esc_attr( $thepostid ); ?>"]</code>
+	<code>[lwcd_timer id="<?php echo esc_html( $thepostid ); ?>"]</code>
 
 	<h4><?php esc_html_e( 'Conditional content before deadline', 'lightweight-countdown' ); ?></h4>
-	<code>[lwcd_before_timer id="<?php echo esc_attr( $thepostid );  ?>"] <?php esc_html_e( 'This only shows before the deadline', 'lightweight-countdown' ); ?> [/lwcd-timer-before]</code>
+	<code>[lwcd_before_timer id="<?php echo esc_html( $thepostid );  ?>"] <?php esc_html_e( 'This only shows before the deadline', 'lightweight-countdown' ); ?> [/lwcd-timer-before]</code>
 
 	<h4><?php esc_html_e( 'Conditional content after deadline', 'lightweight-countdown' ); ?></h4>
-	<code>[lwcd_after_timer id="<?php echo esc_attr( $thepostid ); ?>"] <?php esc_html_e( 'This only shows after the deadline', 'lightweight-countdown' ); ?> [/lwcd-timer-before]</code>
+	<code>[lwcd_after_timer id="<?php echo esc_html( $thepostid ); ?>"] <?php esc_html_e( 'This only shows after the deadline', 'lightweight-countdown' ); ?> [/lwcd-timer-before]</code>
 
 
 </div>
